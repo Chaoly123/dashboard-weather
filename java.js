56d87f9f4a0e4a5dc70bebec2ddf7daddfe08340
Chao Ly <lychaos123@gmail.com>
2021-06-23 21:03:54 -0500

    work on gettting the weather APIkey change and added some coding.

---
+++ java.js
@@ -0,0 +1,34 @@
+var apiKey = "f3e794b6f19299364c3a368c93f4e895"
+var searchBtn = document.getElementById("search")
+
+function mainSearch () {
+    var city = document.getElementById("inputData").value
+    searchCity(city)
+}
+
+function searchCity (city){
+
+    var urlCurrentWeather = "http://api.openweathermap.org/data/2.5/weather?q="+ city +"&appid=" + apiKey + "&units=imperial"
+    
+    fetch(urlCurrentWeather)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(data){
+        console.log(data)
+        console.log(data.main.temp)
+        document.getElementById("cityName").textContent = data.name
+    })
+
+    var urlFiveDay = "http://api.openweathermap.org/data/2.5/forecast?q="+ city +"&appid=" + apiKey + "&units=imperial"
+
+    fetch(urlFiveDay)
+    .then(response => response.json())
+    .then(data => {
+        console.log(data)
+        var noonArr = data.list.filter(index => index.dt_txt.includes("12:00"))
+        console.log(noonArr)
+    })
+}
+
+searchBtn.addEventListener("click", mainSearch)
